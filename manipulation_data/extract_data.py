f87db21d5a1ce4532e116e7ed2cbbd548e82d3a0
joaquin-mineduc <joaquin.astorga@mineduc.cl>
2025-04-16 18:06:18 -0400

    avance 18-04-2025

--- manipulation_data/extract_data.py
+++ manipulation_data/extract_data.py
@@ -4,30 +4,26 @@ from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextContainer
 
 
-
 list_texts = []
 dict_texts = dict()
 for page_layout in extract_pages('C:/Users/joaquin.astorga/mis_proyectos/BAPS/app/inputs/2021/Aula 360.pdf'):
     for element in page_layout:
         if isinstance(element, LTTextContainer):
             list_texts.append(element.get_text())
-
-        
+            #print(element.get_text())
+ 
+data_df = []    
 for index, text in enumerate(list_texts):
-    if index == 11:
-        split_words = text.split("\n")
-
-list_words = []
-for split_word in split_words:
-    words = split_word.split(":")
-    for word in words:
-        list_words.append(word)
-print(list_words)
-        
-
+    if index in [0,1,11,19,22,28,49,50,51,52,85]:
+        if index == 0:
+            data_df.append(text[-5:-1])
+        data_df.append(text)
+    
+print(data_df)
     
 
 
+        
 """directories = os.listdir("app/inputs")
 for directory in directories:
     for file in os.listdir(f'app/inputs/{directory}'):
---
+++ manipulation_data/get_grap.py
@@ -0,0 +1,24 @@
+import fitz  # PyMuPDF
+
+def catch_graphic():
+    pdf_path = 'C:/Users/joaquin.astorga/mis_proyectos/BAPS/app/inputs/2021/Aula 360.pdf'  # Cambia esto por la ruta a tu PDF
+    doc = fitz.open(pdf_path)
+
+    # Recorremos cada página
+    for page_index in range(len(doc)):
+        page = doc[page_index]
+        image_list = page.get_images(full=True)
+        for img_index, img in enumerate(image_list):
+            xref = img[0]
+            if img_index == 6:
+                base_image = doc.extract_image(xref)
+                image_bytes = base_image["image"]
+                image_ext = base_image["ext"]
+                image_filename = f"grafico_p{page_index+1}_{img_index+1}.{image_ext}"   
+                  
+    # Guardar imagen extraída
+    with open(f'app/manipulation_data/grap/{image_filename}', "wb") as f:
+        f.write(image_bytes)      
+    print(f"✅ Imagen extraída y guardada como: {image_filename}")
+
+catch_graphic()
